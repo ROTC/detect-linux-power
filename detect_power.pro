0c986879c98df08b3c61f227901c9112199c1993
丰子小 <2207977085@qq.com>
2014-03-23 19:48:50 +0800

    初步版本

---
+++ detect_power.pro
@@ -0,0 +1,17 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-03-22T19:44:52
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = detect_power
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS  +=
---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <QApplication>
+#include <cstdlib>
+#include <QMessageBox>
+#include <QDialog>
+#include <QDebug>
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QThread>
+#include <QPushButton>
+
+using std::system;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    const int count = 50;
+    QString line;
+    for(int j=0;j!=count;++j)
+    {
+        QThread::sleep(10);
+        system("acpi -V > ~/power.txt");
+        QFile file("/home/rotc/power.txt");
+        if(!file.open(QIODevice::ReadOnly|QIODevice::Text))
+            return -1;
+        QTextStream in(&file);
+        //ifstream power("~/power.txt");
+        for(int i=0;i!=3;++i)
+             line = in.readLine();
+        QString test = line.split(": ").last();
+        if(test=="off-line")
+        {
+            QMessageBox msgBox;
+            msgBox.setText("电源断电了!");
+            msgBox.setInformativeText("你想继续监测电源吗？");
+            QPushButton *yes = msgBox.addButton(QObject::tr("是/Yes"),QMessageBox::YesRole);
+            QPushButton *no = msgBox.addButton(QObject::tr("否/No"),QMessageBox::NoRole);
+            msgBox.setWindowFlags(Qt::WindowStaysOnTopHint);
+            msgBox.setDefaultButton(yes);
+            msgBox.exec();
+            if(msgBox.clickedButton() == yes)
+                continue;
+            else
+                return 0;
+        }
+    }
+    return a.exec();
+}
